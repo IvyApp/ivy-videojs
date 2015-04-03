45120a3d426ba84a6ccfa3c51c633f35da759ca7
Dray Lacy <dray@envylabs.com>
2015-04-03 11:38:37 -0400

    Add ember-template-compiler.js to tests.

--- Brocfile.js
+++ Brocfile.js
@@ -19,6 +19,9 @@ var app = new EmberAddon();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+app.import(path.join(app.bowerDirectory, 'ember/ember-template-compiler.js'), {
+  type: 'test'
+});
 
 var bootstrapTree = pickFiles(path.join(app.bowerDirectory, 'bootstrap/dist/css'), {
   srcDir: '/',
