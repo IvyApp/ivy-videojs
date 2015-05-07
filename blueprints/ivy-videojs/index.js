e17d812833e75600139d5dbc5ec483a43fdb4365
Dray Lacy <dray@envylabs.com>
2015-05-07 15:03:24 -0400

    Constrain video.js version to ~5.0.0-11.

--- blueprints/ivy-videojs/index.js
+++ blueprints/ivy-videojs/index.js
@@ -2,7 +2,7 @@ module.exports = {
   description: 'ivy-videojs',
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('video.js');
+    return this.addBowerPackageToProject('video.js', '~5.0.0-11');
   },
 
   normalizeEntityName: function() {
