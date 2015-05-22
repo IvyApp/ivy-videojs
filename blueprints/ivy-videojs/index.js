7a9f63a4194a85909467cc956f0166b432015fff
Dray Lacy <dray@envylabs.com>
2015-05-22 18:11:06 -0400

    Bump videojs to 5.0.0-21.

--- blueprints/ivy-videojs/index.js
+++ blueprints/ivy-videojs/index.js
@@ -2,7 +2,7 @@ module.exports = {
   description: 'ivy-videojs',
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('video.js', '~5.0.0-11');
+    return this.addBowerPackageToProject('video.js', '~5.0.0-21');
   },
 
   normalizeEntityName: function() {
