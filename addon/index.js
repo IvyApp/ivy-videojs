5d869b33fc2eeb3cc6fe14f2d24e88ab86a9767c
Dray Lacy <dray@envylabs.com>
2015-05-27 14:23:28 -0400

    Basic standalone builds. Not automated at the moment.

---
+++ addon/index.js
@@ -0,0 +1,7 @@
+import IvyVideojsComponent from './components/ivy-videojs';
+import IvyVideojsSourceComponent from './components/ivy-videojs-source';
+
+export {
+  IvyVideojsComponent,
+  IvyVideojsSourceComponent
+};
