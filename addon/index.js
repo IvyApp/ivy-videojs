9badb6719f4d0167d5eb96d879165e169fc67105
Dray Lacy <dray@envylabs.com>
2015-05-27 15:10:18 -0400

    Revert "Basic standalone builds. Not automated at the moment."
    This reverts commit 5d869b33fc2eeb3cc6fe14f2d24e88ab86a9767c.

--- addon/index.js
+++
@@ -1,7 +0,0 @@
-import IvyVideojsComponent from './components/ivy-videojs';
-import IvyVideojsSourceComponent from './components/ivy-videojs-source';
-
-export {
-  IvyVideojsComponent,
-  IvyVideojsSourceComponent
-};
