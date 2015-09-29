019d866333b9bbd78bc4837d03b8efc9cdd8025b
Dray Lacy <dray@envylabs.com>
2015-09-29 14:26:04 -0400

    Upgrade to ember-cli 1.13.8.

---
+++ ember-cli-build.js
@@ -0,0 +1,33 @@
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-addon');
+var path = require('path');
+var pickFiles = require('broccoli-static-compiler');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    // Add options here
+  });
+
+  /*
+    This build file specifes the options for the dummy test app of this
+    addon, located in `/tests/dummy`
+    This build file does *not* influence how the addon or the app using it
+    behave. You most likely want to be modifying `./index.js` or app's build file
+  */
+
+  app.import(path.join(app.bowerDirectory, 'ember/ember-template-compiler.js'), {
+    type: 'test'
+  });
+
+  var bootstrapTree = pickFiles(path.join(app.bowerDirectory, 'bootstrap/dist/css'), {
+    srcDir: '/',
+    destDir: '/assets'
+  });
+
+  var fixturesTree = pickFiles(path.join(__dirname, 'tests/fixtures'), {
+    srcDir: '/',
+    destDir: '/assets'
+  });
+
+  return app.toTree([bootstrapTree, fixturesTree]);
+};
