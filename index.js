91aa3e365e799c785c5fdcb7131db6cfd11d76dc
Ryan Tablada <ryan.tablada@gmail.com>
2016-05-31 17:55:12 -0400

    Don't import on fastboot builds

--- index.js
+++ index.js
@@ -11,33 +11,35 @@ module.exports = {
   },
 
   included: function(app) {
-    var options = app.options.videojs || {};
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+      var options = app.options.videojs || {};
 
-    app.import({
-      development: path.join(app.bowerDirectory, 'video.js/dist/video-js.css'),
-      production:  path.join(app.bowerDirectory, 'video.js/dist/video-js.min.css')
-    });
+      app.import({
+        development: path.join(app.bowerDirectory, 'video.js/dist/video-js.css'),
+        production:  path.join(app.bowerDirectory, 'video.js/dist/video-js.min.css')
+      });
 
-    app.import(path.join(app.bowerDirectory, 'video.js/dist/font/VideoJS.eot'), { destDir: 'assets/font' });
-    app.import(path.join(app.bowerDirectory, 'video.js/dist/font/VideoJS.svg'), { destDir: 'assets/font' });
-    app.import(path.join(app.bowerDirectory, 'video.js/dist/font/VideoJS.ttf'), { destDir: 'assets/font' });
-    app.import(path.join(app.bowerDirectory, 'video.js/dist/font/VideoJS.woff'), { destDir: 'assets/font' });
+      app.import(path.join(app.bowerDirectory, 'video.js/dist/font/VideoJS.eot'), { destDir: 'assets/font' });
+      app.import(path.join(app.bowerDirectory, 'video.js/dist/font/VideoJS.svg'), { destDir: 'assets/font' });
+      app.import(path.join(app.bowerDirectory, 'video.js/dist/font/VideoJS.ttf'), { destDir: 'assets/font' });
+      app.import(path.join(app.bowerDirectory, 'video.js/dist/font/VideoJS.woff'), { destDir: 'assets/font' });
 
-    app.import('vendor/ivy-videojs/shims.js', {
-      exports: {
-        videojs: ['default']
-      }
-    });
+      app.import('vendor/ivy-videojs/shims.js', {
+        exports: {
+          videojs: ['default']
+        }
+      });
 
-    app.import({
-      development: path.join(app.bowerDirectory, 'video.js/dist/video.js'),
-      production:  path.join(app.bowerDirectory, 'video.js/dist/video.min.js')
-    });
+      app.import({
+        development: path.join(app.bowerDirectory, 'video.js/dist/video.js'),
+        production:  path.join(app.bowerDirectory, 'video.js/dist/video.min.js')
+      });
 
-    (options.languages || []).forEach(function(language) {
-      app.import(path.join(app.bowerDirectory, 'video.js/dist/lang/' + language + '.js'));
-    });
+      (options.languages || []).forEach(function(language) {
+        app.import(path.join(app.bowerDirectory, 'video.js/dist/lang/' + language + '.js'));
+      });
 
-    app.import(path.join(app.bowerDirectory, 'video.js/dist/video-js.swf'), { destDir: 'assets' });
+      app.import(path.join(app.bowerDirectory, 'video.js/dist/video-js.swf'), { destDir: 'assets' });
+    }
   }
 };
