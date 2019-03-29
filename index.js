6ce63ea759c649b92e3f2666c546d53e00076d4f
Dray Lacy <dray@envylabs.com>
2019-03-29 17:46:28 -0400

    Remove app.bowerDirectory references in index.js

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ module.exports = {
       var options = app.options.videojs || {};
 
       app.import({
-        development: path.join(app.bowerDirectory, 'video.js/dist/video-js.css'),
-        production:  path.join(app.bowerDirectory, 'video.js/dist/video-js.min.css')
+        development: 'node_modules/video.js/dist/video-js.css',
+        production:  'node_modules/video.js/dist/video-js.min.css'
       });
 
-      app.import(path.join(app.bowerDirectory, 'video.js/dist/font/VideoJS.eot'), { destDir: 'assets/font' });
-      app.import(path.join(app.bowerDirectory, 'video.js/dist/font/VideoJS.svg'), { destDir: 'assets/font' });
-      app.import(path.join(app.bowerDirectory, 'video.js/dist/font/VideoJS.ttf'), { destDir: 'assets/font' });
-      app.import(path.join(app.bowerDirectory, 'video.js/dist/font/VideoJS.woff'), { destDir: 'assets/font' });
+      app.import('node_modules/video.js/dist/font/VideoJS.eot', { destDir: 'assets/font' });
+      app.import('node_modules/video.js/dist/font/VideoJS.svg', { destDir: 'assets/font' });
+      app.import('node_modules/video.js/dist/font/VideoJS.ttf', { destDir: 'assets/font' });
+      app.import('node_modules/video.js/dist/font/VideoJS.woff', { destDir: 'assets/font' });
 
       app.import('vendor/ivy-videojs/shims.js', {
         exports: {
@@ -30,15 +30,15 @@ module.exports = {
       });
 
       app.import({
-        development: path.join(app.bowerDirectory, 'video.js/dist/video.js'),
-        production:  path.join(app.bowerDirectory, 'video.js/dist/video.min.js')
+        development: 'node_modules/video.js/dist/video.js',
+        production:  'node_modules/video.js/dist/video.min.js'
       });
 
       (options.languages || []).forEach(function(language) {
-        app.import(path.join(app.bowerDirectory, 'video.js/dist/lang/' + language + '.js'));
+        app.import('node_modules/video.js/dist/lang/' + language + '.js');
       });
 
-      app.import(path.join(app.bowerDirectory, 'video.js/dist/video-js.swf'), { destDir: 'assets' });
+      app.import('node_modules/video.js/dist/video-js.swf', { destDir: 'assets' });
     }
   }
 };
